d4783bd73d8ca989e5ab483362f6a2ba5d94e660
LucasMcL <LMcLaughlin09@gmail.com>
2017-01-10 13:23:55 -0600

    add event listeners and hide/show sections on click

--- js/app.js
+++ js/app.js
@@ -5,10 +5,27 @@
 // Add event listeners upon page load
 
 $(document).ready(function() {
-
-	$('#tabs li').click((clickEvt) => {
-		console.log(clickEvt)
-	})
+	addTabEvents()
 })
 
-// function onTabClick
+// Hard-coded page navigation based on tab clicking
+function addTabEvents() {
+	$('#watchlist-tab').click(function() {
+		$('#tabs a').removeClass('active')
+		$('#watchlist-tab a').addClass('active')
+		$('section').addClass('hidden')
+		$('#watchlist').removeClass('hidden')
+	})
+	$('#watched-movies-tab').click(function() {
+		$('#tabs a').removeClass('active')
+		$('#watched-movies-tab a').addClass('active')
+		$('section').addClass('hidden')
+		$('#watched-movies').removeClass('hidden')
+	})
+	$('#all-movies-tab').click(function() {
+		$('#tabs a').removeClass('active')
+		$('#all-movies-tab a').addClass('active')
+		$('section').addClass('hidden')
+		$('#all-movies').removeClass('hidden')
+	})
+}
