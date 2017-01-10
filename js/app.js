3ceb8f5985231236ca5350bbccf2ce338c6d106b
LucasMcL <LMcLaughlin09@gmail.com>
2017-01-10 13:04:18 -0600

    working on event listeners

---
+++ js/app.js
@@ -0,0 +1,14 @@
+////////////////////////////
+// App
+////////////////////////////
+
+// Add event listeners upon page load
+
+$(document).ready(function() {
+
+	$('#tabs li').click((clickEvt) => {
+		console.log(clickEvt)
+	})
+})
+
+// function onTabClick
