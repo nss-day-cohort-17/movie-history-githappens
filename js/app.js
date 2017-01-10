9a865626e8beab3f3a098912469a5247dc68db77
Sorrel Brigman <sorrel.k@gmail.com>
2017-01-10 12:48:06 -0600

    added shadow to movie card

---
+++ js/app.js
@@ -0,0 +1,8 @@
+
+//Allows user input field to update upon user focus
+$(document).ready(function() {
+    Materialize.updateTextFields();
+  });
+
+
+//Upon focus of user input, add the "active" class to the label
